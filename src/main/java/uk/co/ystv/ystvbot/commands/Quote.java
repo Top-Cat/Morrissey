db7017441583028b81f3ee1a0d89e5d7b02186c6
Top Cat <webmaster@thomasc.co.uk>
2014-02-02 23:09:42 +0000

    Parameterised sql

--- src/main/java/uk/co/ystv/ystvbot/commands/Quote.java
+++ src/main/java/uk/co/ystv/ystvbot/commands/Quote.java
@@ -15,18 +15,20 @@ public class Quote extends ListenerAdapter<PircBotX> {
 	public void onGenericMessage(GenericMessageEvent<PircBotX> event) throws Exception {
 		if (event.getMessage().startsWith("!quote")) {
 			String query = "";
+			Object[] params = null;
 			if (event.getMessage().length() > 7) {
 				query = event.getMessage().substring(7);
 			}
 			String sqlQuery = "SELECT * FROM quotes";
 			
 			if (query.length() > 0) {
-				sqlQuery += " WHERE quote ILIKE '%" + query + "%' OR description ILIKE '%" + query + "%'";
+				sqlQuery += " WHERE quote ILIKE ? OR description ILIKE ?";
+				params = new Object[] {"%" + query + "%", "%" + query + "%"};
 			}
 			
 			sqlQuery += " ORDER BY RANDOM() * log(id) DESC LIMIT 1";
 			Sql sql = Sql.getInstance();
-			ResultSet result = sql.query(sqlQuery);
+			ResultSet result = sql.query(sqlQuery, params);
 			try {
 				result.next();
 				event.respond("'" + result.getString("quote") + "' - " + result.getString("description"));
